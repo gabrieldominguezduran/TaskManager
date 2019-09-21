5d3c1f0b2c2f9a4075e40cc28f07b823c638693b
Gabriel Domínguez Durán <gabrieldominguezduran@gmail.com>
2019-09-21 17:10:26 +0200

    Add some final features

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <footer id="custom-footer">
-        <span class="mr-4">&copy; 2018 David Acosta</span>
+        <span class="mr-4">&copy; Gabriel Domínguez Durán 2019</span>
         <span class="mr-4">Privacy</span>
         <span>Terms of Service</span>
     </footer>
